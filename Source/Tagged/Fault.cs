3daa6dfc91f0907d2c35b83b1ac2297c1ac8e1ee
Emik <emik.lakunishok@gmail.com>
2022-12-12 09:18:43 +0100

    Move Faults to Tagged

--- Source/Tagged/Fault.cs
+++ Source/Tagged/Fault.cs
@@ -1,7 +1,7 @@
 ﻿// <copyright file="Fault.cs" company="Emik">
 // Copyright (c) Emik. This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // </copyright>
-namespace Emik.Unions;
+namespace Emik.Unions.Tagged;
 
 /// <summary>
 /// A pseudo-exception-like type tossed by some other type, typically as an <see cref="Result{TOk,TErr}.Err"/> value.
